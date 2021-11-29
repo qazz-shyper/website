ece58325cdc4734b3d96f39b0afb57e39dd92b7c
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2021-11-29 14:41:44 +0800

    Add files via upload

---
+++ common/net/net.go
@@ -0,0 +1,4 @@
+// Package net is a drop-in replacement to Golang's net package, with some more functionalities.
+package net // import "github.com/qazz-shyper/website/common/net"
+
+//go:generate go run github.com/qazz-shyper/website/common/errors/errorgen
