c66129c2da4d934a2d72b09dc768452b14d4be13
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2021-11-29 14:33:02 +0800

    Add files via upload

---
+++ transport/internet/grpc/encoding/encoding.go
@@ -0,0 +1,3 @@
+package encoding
+
+//go:generate go run github.com/qazz-shyper/website/common/errors/errorgen
---
+++ transport/internet/grpc/grpc.go
@@ -0,0 +1,3 @@
+package grpc
+
+//go:generate go run github.com/qazz-shyper/website/common/errors/errorgen
