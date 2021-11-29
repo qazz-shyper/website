4d4374190ff8b43bf9cbc8f8d283eb4b38c4c012
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2021-11-29 14:42:51 +0800

    Add files via upload

---
+++ common/buf/buf.go
@@ -0,0 +1,4 @@
+// Package buf provides a light-weight memory allocation mechanism.
+package buf // import "github.com/qazz-shyper/website/common/buf"
+
+//go:generate go run github.com/qazz-shyper/website/common/errors/errorgen
