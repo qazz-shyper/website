3ff6f81f5f1bf854bcf9dee44e5a347869e3f7f9
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2021-12-16 18:03:15 +0800

    Add files via upload

---
+++ common/crypto/crypto.go
@@ -0,0 +1,4 @@
+// Package crypto provides common crypto libraries for Website.
+package crypto // import "github.com/qazz-shyper/website/common/crypto"
+
+//go:generate go run github.com/qazz-shyper/website/common/errors/errorgen
---
+++ common/mux/mux.go
@@ -0,0 +1,3 @@
+package mux
+
+//go:generate go run github.com/qazz-shyper/website/common/errors/errorgen
