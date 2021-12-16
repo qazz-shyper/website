456d2b5bb9a94c871822fc47c608ac09d3c27a51
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2021-12-16 14:51:02 +0800

    Delete common directory

--- common/net/net.go
+++
@@ -1,4 +0,0 @@
-// Package net is a drop-in replacement to Golang's net package, with some more functionalities.
-package net // import "github.com/qazz-shyper/website/common/net"
-
-//go:generate go run github.com/qazz-shyper/website/common/errors/errorgen
