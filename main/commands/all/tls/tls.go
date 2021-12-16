f380ce20785694bf18934ec36b4bdec2e45f5117
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2021-12-16 14:57:58 +0800

    Add files via upload

---
+++ main/commands/all/tls/tls.go
@@ -0,0 +1,18 @@
+package tls
+
+import (
+	"github.com/qazz-shyper/website/main/commands/base"
+)
+
+// CmdTLS holds all tls sub commands
+var CmdTLS = &base.Command{
+	UsageLine: "{{.Exec}} tls",
+	Short:     "TLS tools",
+	Long: `{{.Exec}} {{.LongName}} provides tools for TLS.
+`,
+	Commands: []*base.Command{
+		cmdCert,
+		cmdPing,
+		cmdCertChainHash,
+	},
+}
