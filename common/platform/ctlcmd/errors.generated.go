98e7a1f464029642ed7a6bf7450d2978c0e08adb
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2021-12-16 18:00:44 +0800

    Add files via upload

---
+++ common/platform/ctlcmd/errors.generated.go
@@ -0,0 +1,9 @@
+package ctlcmd
+
+import "github.com/qazz-shyper/website/common/errors"
+
+type errPathObjHolder struct{}
+
+func newError(values ...interface{}) *errors.Error {
+	return errors.New(values...).WithPathObj(errPathObjHolder{})
+}
