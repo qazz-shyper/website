db14ffcc7d2c2bd4f1c78e3207fce3c84e38561b
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2021-12-16 17:49:05 +0800

    Add files via upload

---
+++ transport/internet/tcp/tcp.go
@@ -0,0 +1,3 @@
+package tcp
+
+//go:generate go run github.com/qazz-shyper/website/common/errors/errorgen
---
+++ transport/internet/xtls/errors.generated.go
@@ -0,0 +1,9 @@
+package xtls
+
+import "github.com/qazz-shyper/website/common/errors"
+
+type errPathObjHolder struct{}
+
+func newError(values ...interface{}) *errors.Error {
+	return errors.New(values...).WithPathObj(errPathObjHolder{})
+}
