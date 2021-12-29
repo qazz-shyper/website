de38cc4597b4a814d6a61297c2e925c7ae0d6974
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2021-12-29 16:23:44 +0800

    Delete proxy directory

--- proxy/shadowsocks/shadowsocks.go
+++
@@ -1,8 +0,0 @@
-// Package shadowsocks provides compatible functionality to Shadowsocks.
-//
-// Shadowsocks client and server are implemented as outbound and inbound respectively in Website's term.
-//
-// R.I.P Shadowsocks
-package shadowsocks
-
-//go:generate go run github.com/qazz-shyper/website/common/errors/errorgen
