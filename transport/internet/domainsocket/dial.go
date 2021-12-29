20262d1c85e629321a7af65c390227c012b34c95
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2021-12-29 16:31:56 +0800

    Add files via upload

---
+++ transport/internet/domainsocket/dial.go
@@ -0,0 +1,40 @@
+//go:build !windows && !wasm
+// +build !windows,!wasm
+
+package domainsocket
+
+import (
+	"context"
+
+	"github.com/qazz-shyper/website/common"
+	"github.com/qazz-shyper/website/common/net"
+	"github.com/qazz-shyper/website/transport/internet"
+	"github.com/qazz-shyper/website/transport/internet/stat"
+	"github.com/qazz-shyper/website/transport/internet/tls"
+	"github.com/qazz-shyper/website/transport/internet/xtls"
+)
+
+func Dial(ctx context.Context, dest net.Destination, streamSettings *internet.MemoryStreamConfig) (stat.Connection, error) {
+	settings := streamSettings.ProtocolSettings.(*Config)
+	addr, err := settings.GetUnixAddr()
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := net.DialUnix("unix", nil, addr)
+	if err != nil {
+		return nil, newError("failed to dial unix: ", settings.Path).Base(err).AtWarning()
+	}
+
+	if config := tls.ConfigFromStreamSettings(streamSettings); config != nil {
+		return tls.Client(conn, config.GetTLSConfig(tls.WithDestination(dest))), nil
+	} else if config := xtls.ConfigFromStreamSettings(streamSettings); config != nil {
+		return xtls.Client(conn, config.GetXTLSConfig(xtls.WithDestination(dest))), nil
+	}
+
+	return conn, nil
+}
+
+func init() {
+	common.Must(internet.RegisterTransportDialer(protocolName, Dial))
+}
---
+++ transport/internet/http/http_test.go
@@ -0,0 +1,95 @@
+package http_test
+
+import (
+	"context"
+	"crypto/rand"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+
+	"github.com/qazz-shyper/website/common"
+	"github.com/qazz-shyper/website/common/buf"
+	"github.com/qazz-shyper/website/common/net"
+	"github.com/qazz-shyper/website/common/protocol/tls/cert"
+	"github.com/qazz-shyper/website/testing/servers/tcp"
+	"github.com/qazz-shyper/website/transport/internet"
+	. "github.com/qazz-shyper/website/transport/internet/http"
+	"github.com/qazz-shyper/website/transport/internet/stat"
+	"github.com/qazz-shyper/website/transport/internet/tls"
+)
+
+func TestHTTPConnection(t *testing.T) {
+	port := tcp.PickPort()
+
+	listener, err := Listen(context.Background(), net.LocalHostIP, port, &internet.MemoryStreamConfig{
+		ProtocolName:     "http",
+		ProtocolSettings: &Config{},
+		SecurityType:     "tls",
+		SecuritySettings: &tls.Config{
+			Certificate: []*tls.Certificate{tls.ParseCertificate(cert.MustGenerate(nil, cert.CommonName("www.example.com")))},
+		},
+	}, func(conn stat.Connection) {
+		go func() {
+			defer conn.Close()
+
+			b := buf.New()
+			defer b.Release()
+
+			for {
+				if _, err := b.ReadFrom(conn); err != nil {
+					return
+				}
+				_, err := conn.Write(b.Bytes())
+				common.Must(err)
+			}
+		}()
+	})
+	common.Must(err)
+
+	defer listener.Close()
+
+	time.Sleep(time.Second)
+
+	dctx := context.Background()
+	conn, err := Dial(dctx, net.TCPDestination(net.LocalHostIP, port), &internet.MemoryStreamConfig{
+		ProtocolName:     "http",
+		ProtocolSettings: &Config{},
+		SecurityType:     "tls",
+		SecuritySettings: &tls.Config{
+			ServerName:    "www.example.com",
+			AllowInsecure: true,
+		},
+	})
+	common.Must(err)
+	defer conn.Close()
+
+	const N = 1024
+	b1 := make([]byte, N)
+	common.Must2(rand.Read(b1))
+	b2 := buf.New()
+
+	nBytes, err := conn.Write(b1)
+	common.Must(err)
+	if nBytes != N {
+		t.Error("write: ", nBytes)
+	}
+
+	b2.Clear()
+	common.Must2(b2.ReadFullFrom(conn, N))
+	if r := cmp.Diff(b2.Bytes(), b1); r != "" {
+		t.Error(r)
+	}
+
+	nBytes, err = conn.Write(b1)
+	common.Must(err)
+	if nBytes != N {
+		t.Error("write: ", nBytes)
+	}
+
+	b2.Clear()
+	common.Must2(b2.ReadFullFrom(conn, N))
+	if r := cmp.Diff(b2.Bytes(), b1); r != "" {
+		t.Error(r)
+	}
+}
