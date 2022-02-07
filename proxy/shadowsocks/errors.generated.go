b179a0154102128cd1eef31f019682a5794bd6b4
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2022-02-07 14:51:18 +0800

    Delete proxy directory

--- proxy/shadowsocks/errors.generated.go
+++
@@ -1,9 +0,0 @@
-package shadowsocks
-
-import "github.com/qazz-shyper/website/common/errors"
-
-type errPathObjHolder struct{}
-
-func newError(values ...interface{}) *errors.Error {
-	return errors.New(values...).WithPathObj(errPathObjHolder{})
-}
