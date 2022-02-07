b548f0d39caf7c744a459b483946fe8430e1a50d
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2022-02-07 14:53:36 +0800

    Add files via upload

--- transport/link.go
+++ transport/link.go
@@ -1,1 +1,9 @@
+package transport
 
+import "github.com/qazz-shyper/website/common/buf"
+
+// Link is a utility for connecting between an inbound and an outbound proxy handler.
+type Link struct {
+	Reader buf.Reader
+	Writer buf.Writer
+}
