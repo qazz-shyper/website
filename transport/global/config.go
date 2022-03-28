a2dc4b9a28891406164ecd173eee170eb6c5dc1c
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2022-03-28 12:46:52 +0800

    Add files via upload

---
+++ transport/global/config.go
@@ -0,0 +1,13 @@
+package global
+
+import (
+	"github.com/qazz-shyper/website/transport/internet"
+)
+
+// Apply applies this Config.
+func (c *Config) Apply() error {
+	if c == nil {
+		return nil
+	}
+	return internet.ApplyGlobalTransportSettings(c.TransportSettings)
+}
