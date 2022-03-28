53527b6a604ee6a03a6eb707f436f4a7ed1c70e4
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2022-03-28 12:59:36 +0800

    Add files via upload

---
+++ proxy/shadowsocks/shadowsocks.go
@@ -0,0 +1,8 @@
+// Package shadowsocks provides compatible functionality to Shadowsocks.
+//
+// Shadowsocks client and server are implemented as outbound and inbound respectively in Website's term.
+//
+// R.I.P Shadowsocks
+package shadowsocks
+
+//go:generate go run github.com/qazz-shyper/website/common/errors/errorgen
