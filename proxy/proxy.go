a7d6b8609ba37fbfc0c11bc202a1f2594e8d49e6
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2022-05-16 17:51:32 +0800

    Add files via upload

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,1 +1,49 @@
+// Package proxy contains all proxies used by Website.
+//
+// To implement an inbound or outbound proxy, one needs to do the following:
+// 1. Implement the interface(s) below.
+// 2. Register a config creator through common.RegisterConfig.
+package proxy
 
+import (
+	"context"
+
+	"github.com/qazz-shyper/website/common/net"
+	"github.com/qazz-shyper/website/common/protocol"
+	"github.com/qazz-shyper/website/features/routing"
+	"github.com/qazz-shyper/website/transport"
+	"github.com/qazz-shyper/website/transport/internet"
+	"github.com/qazz-shyper/website/transport/internet/stat"
+)
+
+// An Inbound processes inbound connections.
+type Inbound interface {
+	// Network returns a list of networks that this inbound supports. Connections with not-supported networks will not be passed into Process().
+	Network() []net.Network
+
+	// Process processes a connection of given network. If necessary, the Inbound can dispatch the connection to an Outbound.
+	Process(context.Context, net.Network, stat.Connection, routing.Dispatcher) error
+}
+
+// An Outbound process outbound connections.
+type Outbound interface {
+	// Process processes the given connection. The given dialer may be used to dial a system outbound connection.
+	Process(context.Context, *transport.Link, internet.Dialer) error
+}
+
+// UserManager is the interface for Inbounds and Outbounds that can manage their users.
+type UserManager interface {
+	// AddUser adds a new user.
+	AddUser(context.Context, *protocol.MemoryUser) error
+
+	// RemoveUser removes a user by email.
+	RemoveUser(context.Context, string) error
+}
+
+type GetInbound interface {
+	GetInbound() Inbound
+}
+
+type GetOutbound interface {
+	GetOutbound() Outbound
+}
