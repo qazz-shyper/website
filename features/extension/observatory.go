5edca446ea880442799d405a86897ce139407f72
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2022-05-16 19:03:25 +0800

    Add files via upload

---
+++ features/extension/observatory.go
@@ -0,0 +1,19 @@
+package extension
+
+import (
+	"context"
+
+	"github.com/golang/protobuf/proto"
+
+	"github.com/qazz-shyper/website/features"
+)
+
+type Observatory interface {
+	features.Feature
+
+	GetObservation(ctx context.Context) (proto.Message, error)
+}
+
+func ObservatoryType() interface{} {
+	return (*Observatory)(nil)
+}
